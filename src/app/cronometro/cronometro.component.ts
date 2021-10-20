9a5ba70734798a74e6e42693a838239b326b15cf
Dants96 <danieltutistar@gmail.com>
2021-10-20 17:22:25 -0500

    Ejercicio base teminado

---
+++ src/app/cronometro/cronometro.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-cronometro',
+  templateUrl: './cronometro.component.html',
+  styleUrls: ['./cronometro.component.css']
+})
+export class CronometroComponent implements OnInit {
+  segundo:number=0;
+  @Input() inicio!: string;
+  @Output() multiplo10 = new EventEmitter();
+  constructor() { }
+
+  ngOnInit() {
+    this.segundo = Number(this.inicio);
+    setInterval (()=>{
+      this.segundo++;
+      if (this.segundo % 10 == 0){
+      this.multiplo10.emit(this.segundo);
+    }
+  }, 1000);
+}
+}
